6e15cc8fd51f5c70ba7fcd3dc2ba7096a7949ad9
faheem-Chaudhry <102186300+faheem-Chaudhry@users.noreply.github.com>
2022-05-20 20:04:00 +0500

    3rd commit

---
+++ assignment_1/app/src/main/java/com/example/activitypractice/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.activitypractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onWebb();
+
+            }
+            public void onWebb()
+            {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        } );
+
+
+    }
+}
+
+
