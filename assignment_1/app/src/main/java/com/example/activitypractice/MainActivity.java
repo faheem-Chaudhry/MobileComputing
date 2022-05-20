3352fbedd8a32c8440ede68275774efda2af7ee9
faheem-Chaudhry <102186300+faheem-Chaudhry@users.noreply.github.com>
2022-05-20 20:13:07 +0500

    2nd commit in assignment

--- assignment_1/app/src/main/java/com/example/activitypractice/MainActivity.java
+++ assignment_1/app/src/main/java/com/example/activitypractice/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.button);
+        btn = findViewById(R.id.A);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
