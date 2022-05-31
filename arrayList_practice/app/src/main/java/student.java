128664e40e74c4058c145957c067eb45a6ed1588
faheem-Chaudhry <102186300+faheem-Chaudhry@users.noreply.github.com>
2022-05-31 08:45:55 +0500

    class added

---
+++ arrayList_practice/app/src/main/java/student.java
@@ -0,0 +1,25 @@
+public class student<string> {
+    int id;
+    string name;
+
+    public student(int id, string name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+}
